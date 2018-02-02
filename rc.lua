698c9ded5e92d9847d120eb45feedc53ece868f9
Georg Felbinger <gfelbing@github.com>
2018-02-02 10:15:42 +0100

    remove ugly ff rule

--- rc.lua
+++ rc.lua
@@ -653,10 +653,6 @@ awful.rules.rules = {
     { rule_any = { type = { "dialog", "normal" } },
       properties = { titlebars_enabled = false } },
 
-    -- Set Firefox to always map on the first tag on screen 1.
-    { rule = { class = "Firefox" },
-      properties = { screen = 1, tag = awful.util.tagnames[1] } },
-
     { rule = { class = "Gimp", role = "gimp-image-window" },
           properties = { maximized = true } },
 }
