9665dd5623a0721a0961a497b0a94e1d9cecc4e5
Georg Felbinger <georg.felbinger@freiheit.com>
2020-01-03 08:34:26 +0100

    add nm-applet

--- rc.lua
+++ rc.lua
@@ -56,7 +56,7 @@ local function run_once(cmd_arr)
     end
 end
 
-run_once({ "urxvtd", "unclutter -root" }) -- entries must be separated by commas
+run_once({ "urxvtd", "unclutter -root", "nm-applet" }) -- entries must be separated by commas
 
 -- This function implements the XDG autostart specification
 --[[
